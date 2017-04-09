6e423cf6e240b83a73c99487517dec69160eb727
Ivan Avdeev <me@w23.ru>
2017-04-10 00:21:31 +0700

    a really bad path tracer

--- raymarch.glsl
+++ raymarch.glsl
@@ -6,7 +6,6 @@ const float PI = 3.14159265359;
 
 float hash(vec2 p){return fract(1e4*sin(17.*p.x+.1*p.y)*(.1+abs(sin(13.*p.y+p.x))));}
 float hash(vec3 p){return hash(vec2(hash(p.xy), p.z));}
-
 float noise(vec3 p){
 	vec3 P=floor(p);p=p-P;
 	p*=p*(3.-2.*p);
@@ -26,77 +25,20 @@ vec3 N(vec3 p) {
 	return normalize(vec3(W(p+E.yxx)-w,W(p+E.xyx)-w,W(p+E.xxy)-w));
 }
 
-float DistributionGGX(vec3 N, vec3 H, float roughness) {
-    float a      = roughness*roughness;
-    float a2     = a*a;
-    float NdotH  = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
-    float nom   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-    return nom / denom;
-}
-float GeometrySchlickGGX(float NdotV, float roughness) {
-    float r = (roughness + 1.0);
-    float k = (r*r) / 8.0;
-    float nom   = NdotV;
-    float denom = NdotV * (1.0 - k) + k;
-    return nom / denom;
-}
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness) {
-    float NdotV = max(dot(N, V), 0.0);
-    float NdotL = max(dot(N, L), 0.0);
-    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
-    return ggx1 * ggx2;
-}
-vec3 fresnelSchlick(float cosTheta, vec3 F0) {
-    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-float RadicalInverse_VdC(uint bits) {
-	bits = (bits << 16u) | (bits >> 16u);
-	bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
-	bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
-	bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
-	bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
-	return float(bits) * 2.3283064365386963e-10; // / 0x100000000
-}
-vec2 Hammersley(uint i, uint N) {
-	return vec2(float(i)/float(N), RadicalInverse_VdC(i));
-}
-
-vec3 ImportanceSampleGGX(vec2 Xi, vec3 N, float roughness) {
-	float a = roughness*roughness;
-
-	float phi = 2.0 * PI * Xi.x;
-	float cosTheta = sqrt((1.0 - Xi.y) / (1.0 + (a*a - 1.0) * Xi.y));
-	float sinTheta = sqrt(1.0 - cosTheta*cosTheta);
-
-	// from spherical coordinates to cartesian coordinates - halfway vector
-	vec3 H;
-	H.x = cos(phi) * sinTheta;
-	H.y = sin(phi) * sinTheta;
-	H.z = cosTheta;
-
-	//from tangent-space H vector to world-space sample vector
-	vec3 up        = abs(N.z) < 0.999 ? vec3(0.0, 0.0, 1.0) : vec3(1.0, 0.0, 0.0);
-	vec3 tangent   = normalize(cross(up, N));
-	vec3 bitangent = cross(N, tangent);
-
-	vec3 sampleVec = tangent * H.x + bitangent * H.y + N * H.z;
-	return normalize(sampleVec);
-}
-
-void M(vec3 p, out vec3 albedo, out float metallic, out float roughness) {
-	albedo = vec3(step(p.y,-.9), 1., 1.);
-	metallic = .9;
-	roughness = noise(p*3.);
+void M(vec3 p, out vec3 albedo, out float roughness) {
+	if (p.y < -.9) albedo = vec3(1.); else {
+		if (p.x < -.9) albedo = vec3(1.,0.,0.); else
+		if (p.x > .9) albedo = vec3(0.,0.,1.); else
+		albedo = vec3(0.,1.,0.);
+	}
+	roughness = 1.;
+	//roughness = min(1.,.1+pow(noise(p*3.), 4.));
 }
 
 const float ML = 10.;
 vec3 LP[2], LC[2];
 
-bool raytrace(vec3 o, vec3 d, out vec3 p, out vec3 n, out vec3 c, out vec3 albedo, out float metallic, out float roughness) {
+bool raytrace(vec3 o, vec3 d, out vec3 p, out vec3 n, out vec3 c, out vec3 albedo, out float roughness) {
 	float l = 0.;
 	for (int i = 0; i < 64; ++i) {
 		p = o+d*l;
@@ -109,38 +51,22 @@ bool raytrace(vec3 o, vec3 d, out vec3 p, out vec3 n, out vec3 c, out vec3 albed
 	if (l > ML) return false;
 
 	n = N(p);
-	M(p, albedo, metallic, roughness);
+	M(p, albedo, roughness);
 	d = -d;
 
-	for(int i = 0; i < 2; ++i)
-	{
-		vec3 L = normalize(LP[i] - p);
-		float distance    = length(LP[i] - p);
-
+	p += n * .01;
+	for(int i = 0; i < 2; ++i) {
+		vec3 L = LP[i] - p;
 		float shadow = 1.;
 		for (int j = 0; j < 8; ++j) {
-			float w=W(p + n * .01 + L * distance * float(j+1) / 8.);
+			float w=W(p + L * float(j+1) / 8.);
 			shadow = min(shadow, w);
 		}
 
-		if (shadow < .01) break;
+		if (shadow < .0001) break;
 
-		vec3 H = normalize(d + L);
-		float attenuation = 1.0 / (distance * distance);
-		vec3 radiance     = LC[i] * attenuation;
-		vec3 F0 = mix(vec3(.04), albedo, metallic);
-		vec3 F  = fresnelSchlick(max(dot(H, d), 0.0), F0);
-		float NDF = DistributionGGX(n, H, roughness);
-		float G   = GeometrySmith(n, d, L, roughness);
-		float NdotL = max(dot(n, L), 0.0);
-		vec3 nominator    = NDF * G * F;
-		float denominator = 4. * max(dot(n, d), 0.0) * NdotL + 0.001;
-		vec3 brdf         = nominator / denominator;
-
-		vec3 kS = F;
-		vec3 kD = vec3(1.0) - kS;
-		kD *= 1.0 - metallic;
-		c += (kD * albedo / PI + brdf) * radiance * NdotL;
+		float dL = max(0.,dot(n,normalize(L)));
+		c += dL * albedo * LC[i] / dot(L,L);
 	}
 
 	return true;
@@ -159,29 +85,35 @@ void main() {
 	vec3 D = normalize(vec3(uv, -2.));
 
 	vec3 color,p,n,albedo;
-	float m,r;
-	raytrace(O, D, p, n, color, albedo, m, r);
+	float r;
+	raytrace(O, D, p, n, color, albedo, r);
 
+	//r = 1.;
 	float sum = 0.;
 	vec3 col = vec3(0.);
-	const uint SAMPLE_COUNT = 16u;
-	uint dj = uint(hash(p)*float(SAMPLE_COUNT));
-	for (uint j = 0u; j < SAMPLE_COUNT; ++j) {
-		vec2 Xi = Hammersley(j+dj, 2u*SAMPLE_COUNT);
-		vec3 H  = ImportanceSampleGGX(Xi, n, r);
-		vec3 L2  = normalize(2.*dot(-D, H)*H+D);
-		float NdotL2 = max(dot(n, L2), 0.0);
-		if(NdotL2 > 0.0)
-		{
-			vec3 p2,n2,c2,a2;
-			float m2,r2;
-			raytrace(p+n*.01, L2, p2, n2, c2, a2, m2, r2);
-			col += c2 * NdotL2;
-			sum += NdotL2;
+	const int SAMPLE_COUNT = 1;
+	vec3 nr = reflect(D,n);
+	for (int j = 0; j < SAMPLE_COUNT; ++j) {
+		float fj = float(j)+T;
+		float rx = r * (hash(vec2(p.x, fj)) * 2. - 1.);
+		float ry = r * (hash(vec2(p.y, fj)) * 2. - 1.);
+		float rz = sqrt(1. - rx*rx - ry*ry);
+
+		vec3 tg = abs(nr.z) > .9 ? E.zxx : E.xxz, btg = normalize(cross(nr,tg));
+		tg = cross(btg,nr);
+		vec3 dd = nr * rz + tg * rx + btg * ry;
+		float k = max(0.,dot(dd,n));
+
+		vec3 p2,n2,c2,a2;
+		float r2;
+		if (k > 0. && raytrace(p, dd, p2, n2, c2, a2, r2)) {
+			col += c2;// * k;
+			sum += 1.;
 		}
 	}
 
-	color += col * albedo / sum;
+	if (sum > 0.)
+		color += col * albedo / sum;
 	//color = col;
 
 	color = color / (color + vec3(1.0));
